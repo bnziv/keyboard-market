f34c09333df48251ea3e90a30ace2637eee2ff85
Benzi <benzi.villaruel@gmail.com>
2025-02-10 20:50:42 -0500

    Remove security and application.properties tracking

--- backend/src/main/java/com/keyboardmarket/KeyboardMarketBackendApplication.java
+++ backend/src/main/java/com/keyboardmarket/KeyboardMarketBackendApplication.java
@@ -2,8 +2,9 @@ package com.keyboardmarket;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = SecurityAutoConfiguration.class)
 public class KeyboardMarketBackendApplication {
 
 	public static void main(String[] args) {
