08f163c73cb290f9ca7368ceeeb93cfb14bfc774
Benzi <benzi.villaruel@gmail.com>
2025-02-10 21:17:27 -0500

    Add password hashing

--- backend/src/main/java/com/keyboardmarket/service/UserService.java
+++ backend/src/main/java/com/keyboardmarket/service/UserService.java
@@ -1,15 +1,19 @@
 package com.keyboardmarket.service;
 
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 import com.keyboardmarket.model.User;
 import com.keyboardmarket.repository.UserRepository;
+import com.mongodb.DuplicateKeyException;
+
 import lombok.RequiredArgsConstructor;
 
 @Service
 @RequiredArgsConstructor
 public class UserService {
     private final UserRepository userRepository;
+    private final PasswordEncoder passwordEncoder;
 
     public User registerUser(User user) {
         if (userRepository.existsByEmailIgnoreCase(user.getEmail())) {
@@ -19,6 +23,13 @@ public class UserService {
         if (userRepository.existsByUsernameIgnoreCase(user.getUsername())) {
             throw new RuntimeException("Username '" + user.getUsername() + "' is already in use");
         }
-        return userRepository.save(user);
+
+        String hashedPassword = passwordEncoder.encode(user.getPassword());
+        user.setPassword(hashedPassword);
+        try {
+            return userRepository.save(user);
+        } catch (DuplicateKeyException e) { // Edge case if it passes the previous checks
+            throw new RuntimeException("User already exists");
+        }
     }
 }
