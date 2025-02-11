43aee7fcb6c2d6b3bb27e3f447dbe64c73468bc7
Benzi <benzi.villaruel@gmail.com>
2025-02-10 19:58:29 -0500

    Initial Spring Boot commit

---
+++ backend/src/test/java/com/keyboardmarket/KeyboardMarketBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.keyboardmarket;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KeyboardMarketBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
