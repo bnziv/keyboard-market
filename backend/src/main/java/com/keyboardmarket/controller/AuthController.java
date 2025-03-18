94f253ffb5ae9dd27da994da7d4bbdb73d842a22
Benzi <benzi.villaruel@gmail.com>
2025-03-17 20:32:22 -0400

    Set SameSite in cookie

--- backend/src/main/java/com/keyboardmarket/controller/AuthController.java
+++ backend/src/main/java/com/keyboardmarket/controller/AuthController.java
@@ -56,6 +56,7 @@ public class AuthController {
         cookie.setPath("/");
         cookie.setHttpOnly(true);
         cookie.setSecure(true);
+        cookie.setAttribute("SameSite", "None");
         response.addCookie(cookie);
         
         return ResponseEntity.ok().build();
@@ -81,6 +82,7 @@ public class AuthController {
         cookie.setPath("/");
         cookie.setHttpOnly(true);
         cookie.setSecure(true);
+        cookie.setAttribute("SameSite", "None");
         response.addCookie(cookie);
     }
 }
